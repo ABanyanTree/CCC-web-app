9e1d840924a251e951b98c21fddbc2359c2a4679
Shahen9119 <shahen.sayyad@gmail.com>
2022-11-08 19:44:14 +0530

    some work

--- CCC/ViewModels/Response/GetAllPetDataResponse.cs
+++ CCC/ViewModels/Response/GetAllPetDataResponse.cs
@@ -7,5 +7,33 @@ namespace CCC.UI.ViewModels
 {
     public class GetAllPetDataResponse
     {
+        public string ServiceId { get; set; }
+        public string PetId { get; set; }
+        public bool Gender { get; set; }
+        public string CertificateNo { get; set; }
+        public string TagId { get; set; }
+        public DateTime EntryDate { get; set; }
+        public string CenterId { get; set; }
+        public string AreaId { get; set; }
+        public string CareGiver { get; set; }
+        public string VetId { get; set; }
+        public DateTime? SurgeryDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string Complications { get; set; }
+        public bool? IsExpired { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public string CreatorName { get; set; }
+        public string ModifiorName { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public string SortExp { get; set; }
     }
 }
