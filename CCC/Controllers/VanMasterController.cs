bffcc2a372eba000959e313b1ee12adf8664b655
Shahen9119 <shahen.sayyad@gmail.com>
2022-11-16 21:29:46 +0530

    Manage section design done for center and vet

---
+++ CCC/Controllers/VanMasterController.cs
@@ -0,0 +1,22 @@
+﻿using CCC.UI.RefitClientFactory;
+using CCC.UI.Utility;
+using CCC.UI.ViewModels;
+using DataTables.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace CCC.UI.Controllers
+{
+    public class VanMasterController : Controller
+    {
+        public async Task<ActionResult> ManageVan()
+        {
+            VanMasterRequest obj = new VanMasterRequest();
+            return View(obj);
+        }
+    }
+}
